6a1bf5128e292a570b60352eeb26841da2564f2a
amureo <wkeehd12@gmail.com>
2023-01-25 23:49:14 +0900

    chap01 sec03 : add command in Hello.java

--- chap01/src/sec03/exam01/Hello.java
+++ chap01/src/sec03/exam01/Hello.java
@@ -1,10 +1,15 @@
 package sec03.exam01;
 
-public class Hello {
+/*
+ * 작성자 : amureo
+ * 작성일 : 2023.01.25
+ */
 
+// 프로그램 실행 진입점
+public class Hello {
+	// 콘솔에 출력하는 실행문
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Java");
-	}
-
-}
+	} // end of main
+} // end of class
