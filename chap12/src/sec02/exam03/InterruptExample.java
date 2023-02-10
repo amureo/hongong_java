2fa2f007ccc8a376fe18c248f134d8f7679d4e63
amureo <wkeehd12@gmail.com>
2023-02-10 15:21:22 +0900

    ch12 sec02

---
+++ chap12/src/sec02/exam03/InterruptExample.java
@@ -0,0 +1,12 @@
+package sec02.exam03;
+
+public class InterruptExample {
+
+	public static void main(String[] args) {
+		PrintThread2 printThread=new PrintThread2();
+		printThread.start();
+		try {Thread.sleep(1000);}catch(InterruptedException e) {}
+		printThread.interrupt();
+	}
+
+}
