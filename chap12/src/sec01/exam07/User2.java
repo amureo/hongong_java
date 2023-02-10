d17deeb7ba8f838f94b8e76cab02fa270dd884b6
amureo <wkeehd12@gmail.com>
2023-02-10 14:48:34 +0900

    ch12 sec01

---
+++ chap12/src/sec01/exam07/User2.java
@@ -0,0 +1,13 @@
+package sec01.exam07;
+
+public class User2 extends Thread{
+	private Calculator calculator;
+	
+	public void setCalculator(Calculator calculator) {
+		this.setName("User2");
+		this.calculator=calculator;
+	}
+	public void run() {
+		calculator.setMemory(50);
+	}
+}
